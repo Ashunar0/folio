5411aa46936d051c5bddd5ce6f7a4fddced8bbde
Ashunar0 <asahi.gaia1530@gmail.com>
2025-12-05 22:31:43 +0900

    fixed logout

--- app/(auth)/logout/page.tsx
+++ app/(auth)/logout/page.tsx
@@ -1,14 +1,10 @@
-"use client";
-
 import { signOut } from "@/lib/auth/api";
 import { createSupabaseBrowserClient } from "@/lib/supabase-browser";
-import { useRouter } from "next/navigation";
 
 export default function LogoutPage() {
-  const router = useRouter();
   const handleLogout = async () => {
     await signOut(createSupabaseBrowserClient());
-    router.push("/login");
+    window.location.href = "/login";
   };
 
   handleLogout();
